2aa926ac1748a28d79aded1341499d35d30f7340
theblockcoder <51024479+theblockcoder@users.noreply.github.com>
2024-08-26 15:37:39 +0100

    Added error so application gracefully exits when it can't parse the CSV

--- barchart/src/csvparser.cpp
+++ barchart/src/csvparser.cpp
@@ -11,48 +11,56 @@ CsvParser::CsvParser(const std::string& fileName)
   if (!csvFile.is_open())
     throw std::runtime_error("Failed to open CSV file");
 
-  // Read in the categories row
-  std::getline(csvFile, line);
-
-  // Read in the title
-  auto it = std::find(line.begin(), line.end(), ',');
-  name = std::string(line.begin(), it);
-  // Read in the categories
-  auto prevIt = it;
-  while (it != line.end())
+  try
   {
-    it = std::find(prevIt+1, line.end(), ',');
-    std::string category(prevIt+1, it);
-    if (!category.empty())
-      categories.push_back(std::string(prevIt+1, it));
+    // Read in the categories row
+    std::getline(csvFile, line);
 
-    prevIt = it;
-  }
-
-  // Read in each row
-  while (std::getline(csvFile, line))
-  {
-    Row r;
-    
     // Read in the title
     auto it = std::find(line.begin(), line.end(), ',');
-    r.name = std::string(line.begin(), it);
-
-    // Read in the values
+    name = std::string(line.begin(), it);
+    // Read in the categories
     auto prevIt = it;
     while (it != line.end())
     {
       it = std::find(prevIt+1, line.end(), ',');
-      std::string value(prevIt+1, it);
-      if (!value.empty())
-        r.values.push_back(std::stold(value));
+      std::string category(prevIt+1, it);
+      if (!category.empty())
+        categories.push_back(std::string(prevIt+1, it));
       else
-        // If the value is blank, i.e. 2 adjacent commas, enter 0
-        r.values.push_back(0);
+        throw std::exception();
 
       prevIt = it;
     }
 
-    rows.push_back(std::move(r));
+    // Read in each row
+    while (std::getline(csvFile, line))
+    {
+      Row r;
+
+      // Read in the title
+      auto it = std::find(line.begin(), line.end(), ',');
+      r.name = std::string(line.begin(), it);
+
+      // Read in the values
+      auto prevIt = it;
+      while (it != line.end())
+      {
+        it = std::find(prevIt+1, line.end(), ',');
+        std::string value(prevIt+1, it);
+        if (!value.empty())
+          r.values.push_back(std::stold(value));
+        else
+          // If the value is blank, i.e. 2 adjacent commas, enter 0
+          r.values.push_back(0);
+
+        prevIt = it;
+      }
+
+      rows.push_back(std::move(r));
+    }
+  } catch (std::exception& e)
+  {
+    throw std::runtime_error("Failed to parse/understand CSV file, are you sure its valid!");
   }
 }
