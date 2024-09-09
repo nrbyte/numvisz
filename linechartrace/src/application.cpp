974e609f6755390ecead3d81855bfd4c55b7fce4
Neel R <51024479+nbytr@users.noreply.github.com>
2024-09-09 17:23:22 +0100

    Now draws the title along the top, current category along the bottom, and row data along the side

--- linechartrace/src/application.cpp
+++ linechartrace/src/application.cpp
@@ -5,6 +5,7 @@
 
 #include "viszbase/linerenderer.hpp"
 #include "viszbase/renderer.hpp"
+#include "viszbase/fontrenderer.hpp"
 #include "viszbase/math.hpp"
 #include "viszbase/csvparser.hpp"
 #include "viszbase/timer.hpp"
@@ -48,6 +49,8 @@ int Application::run()
 
     Timer timer;
     Renderer renderer;
+    FontRenderer fontRenderer;
+    FontRenderer fontRendererLarge;
     math::Matrix<4, 4> proj;
 
     // Parse the provided CSV
@@ -60,10 +63,20 @@ int Application::run()
     // sensible default
     Timer::FloatMS timePerCategory{args.getInt("-timepercategory", 100)};
 
+    // Store number of decimal places for drawing numbers
+    int numOfDecimalPlaces = args.getInt("-decimalplaces", 0);
+
     // Get line thickness from arguments if set, otherwise use
     // sensible default
     float lineThickness = args.getInt("-linethickness", 10);
     lineThickness /= 1000;
+
+    // Load the provided font file
+    const std::string& fontName = args.get("-font");
+    if (fontName == Arguments::NotSet)
+        throw std::runtime_error("Font file not provided");
+    fontRenderer.loadFont(fontName, 18);
+    fontRendererLarge.loadFont(fontName, 26);
 
     // Padding and Spacing values
     struct
@@ -81,12 +94,14 @@ int Application::run()
         unsigned afterLines;
     } Spacings;
     Spacings.beforeLines = Paddings.beforeLines;
-    Spacings.aboveLines = Paddings.aboveLines;
-    Spacings.belowLines = Paddings.belowLines;
+    Spacings.aboveLines =
+        Paddings.aboveLines + fontRendererLarge.getFontHeight();
+    Spacings.belowLines = Paddings.belowLines + fontRenderer.getFontHeight();
     Spacings.afterLines = Paddings.afterLines;
 
     // Go through each line, and load in the values
     std::vector<Line> lines;
+    std::string longestRowName = "";
     for (auto& row : csv.getRows())
     {
         LineRendererBuilder builder;
@@ -99,7 +114,17 @@ int Application::run()
         }
         lines.push_back(
             {row.name, Color{1.0f, 1.0f, 1.0f, 1.0f}, builder.build(), 0.0f});
+
+        // Update longest row name
+        if (row.name.size() > longestRowName.size())
+            longestRowName = row.name;
     }
+
+    // Update spacing based on longest row name
+    float newSpacingAfterLines =
+        fontRenderer.getWidthOfMsg(longestRowName) + Paddings.afterLines;
+    if (Spacings.afterLines < newSpacingAfterLines)
+        Spacings.afterLines = newSpacingAfterLines;
 
     // Generate colours
     generateColors(lines);
@@ -150,20 +175,29 @@ int Application::run()
             std::find_if(lines.begin(), lines.end(),
                          [&](const auto& l) { return l.name == row.name; })
                 ->currentValue = currentValue;
-
-            // Update the height if necessary
-            if (currentValue > height)
-                height = currentValue;
         }
+        std::sort(lines.begin(), lines.end(), [](const auto& x, const auto& y)
+                  { return x.currentValue > y.currentValue; });
+
+        // Update height if necessary
+        if (lines.front().currentValue > height)
+            height = lines.front().currentValue;
+
+        // Update spacing if necessary
+        newSpacingAfterLines =
+            Paddings.afterLines +
+            fontRenderer.getWidthOfLongDouble(lines.front().currentValue,
+                                              numOfDecimalPlaces);
+        if (Spacings.afterLines < newSpacingAfterLines)
+            Spacings.afterLines = newSpacingAfterLines;
 
         // Set projection
         math::setOrtho(proj, height * (1 + lineThickness / 2),
                        currentTime.count(), 0, 0, -0.1f, -100.0f);
         // Update viewport to leave space around the lines
-        gui.setViewport(
-            Spacings.beforeLines, Spacings.aboveLines,
-            (gui.width - Spacings.afterLines) - Spacings.beforeLines,
-            (gui.height - Spacings.belowLines) - Spacings.aboveLines);
+        gui.setViewport(Spacings.beforeLines, Spacings.belowLines,
+                        gui.width - Spacings.afterLines - Spacings.beforeLines,
+                        gui.height - Spacings.aboveLines - Spacings.belowLines);
         // Draw the lines
         float aspectRatio = float(gui.width) / gui.height;
         for (auto& line : lines)
@@ -187,6 +221,40 @@ int Application::run()
                          gui.width - Spacings.afterLines,
                          gui.height - Spacings.belowLines + 1,
                          Color{0, 0, 0, 1}, proj);
+
+        // Draw text
+        // Draw title
+        fontRendererLarge.drawMsg(Spacings.beforeLines,
+                                  Paddings.aboveLines / 2.0, csv.getName(),
+                                  proj);
+        // Draw categories along the bottom
+        fontRenderer.drawMsg(Spacings.beforeLines,
+                             gui.height - Spacings.belowLines +
+                                 Paddings.belowLines * 0.2,
+                             csv.getCategories()[0], proj);
+        const std::string& currentCategory =
+            csv.getCategories()[intCurrentPosition];
+        fontRenderer.drawMsg(gui.width - Spacings.afterLines -
+                                 fontRenderer.getWidthOfMsg(currentCategory),
+                             gui.height - Spacings.belowLines +
+                                 Paddings.belowLines * 0.2,
+                             currentCategory, proj);
+
+        // Draw row names and values next to lines
+        for (const auto& line : lines)
+        {
+            float lineY =
+                Spacings.aboveLines +
+                (1 - line.currentValue / height) *
+                    (gui.height - Spacings.aboveLines - Spacings.belowLines);
+            fontRenderer.drawMsg(
+                gui.width - Spacings.afterLines + Paddings.afterLines * 0.2,
+                lineY - fontRenderer.getFontHeight() / 2.0, line.name, proj);
+            fontRenderer.drawLongDouble(
+                gui.width - Spacings.afterLines + Paddings.afterLines * 0.2,
+                lineY + fontRenderer.getFontHeight() * 0.3, line.currentValue,
+                numOfDecimalPlaces, proj);
+        }
 
         // Advance to the next frame
         gui.nextFrame();
